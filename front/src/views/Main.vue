3cbbc59a23174a326e3f3ca4def0e9fbc5664ac5
rokealvo <rokealvo@gmail.com>
2021-03-20 21:10:25 +0700

    added login page

---
+++ front/src/views/Main.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container class="login-page d-flex align-center justify-center">
+    <v-card max-width="360" width="100%">
+      <v-container>
+        <h1>login form</h1>
+      </v-container>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "LoginPage",
+  components: {},
+};
+</script>
+
+<style lang="scss">
+.login-page {
+  min-height: 100%;
+  background-color: lightgray;
+}
+</style>
